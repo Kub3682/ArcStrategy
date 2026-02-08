d6a06e5b8abcce078b13525545ccefcbcc182e13
Kub <435086503@qq.com>
2026-02-08 20:07:42 +0800

    进一步细节优化点的补充注释说明，实现非阻塞逻辑

--- Arc41/LED/Fun/blink.c
+++ Arc41/LED/Fun/blink.c
@@ -22,3 +22,7 @@ void LightFunc_Blink(void)
     Led_DD_SetState(DIO_STATE_HIGH);
     DelayMs(200);
 }
+
+//以上，为了简便，暂采取使用了 for 循环死等（Busy-wait），在闪烁期间，CPU 被锁死在循环里，无法响应其他任务（除非用中断）。
+//后续工程优化，可考虑引入一个 Tick 计数器，让闪烁逻辑变成“状态机”。例如：if (currentTime - lastTime > 200ms) { toggleLED; }。这样 Led_Ctrl_Handle 就能实现真正的并发。
+//如此，可实现非阻塞逻辑
